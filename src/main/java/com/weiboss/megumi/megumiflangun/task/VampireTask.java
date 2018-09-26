82081d07e25844e0b000253acfdcf45399c4d286
Wei_Boss <1731598625@qq.com>
2018-09-26 17:33:32 +0800

    增加吸血效果特性
    Signed-off-by: Wei_Boss <1731598625@qq.com>

--- src/main/java/com/weiboss/megumi/megumiflangun/task/VampireTask.java
+++ src/main/java/com/weiboss/megumi/megumiflangun/task/VampireTask.java
@@ -5,10 +5,24 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class VampireTask extends BukkitRunnable {
     private LivingEntity entity;
-    private double vampire;
+    private Double vampire;
+    private Integer amount;
+
+    public VampireTask(LivingEntity entity, Double vampire, Integer amount) {
+        this.entity = entity;
+        this.vampire = vampire;
+        this.amount = amount;
+    }
 
     @Override
     public void run() {
-
+        if (amount <= 0) {
+            cancel();
+            return;
+        }
+        double heath = entity.getHealth() + (vampire / amount);
+        if (heath > entity.getMaxHealth()) entity.setHealth(entity.getMaxHealth());
+        else entity.setHealth(heath);
+        amount--;
     }
 }
